1098d9abfc91bef4bd7b9bb7f73ae78d8387d0d3
psanjayvarma <82930867+psanjayvarma@users.noreply.github.com>
2021-05-19 13:04:02 +0530

    updated app.js to run on 8000 port

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-var express = require('express')
+var express = require("express");
 
 var app = express();
+var port = 8000;
 
 var eventRouter = express.Router();
 
-eventRouter.route('/events')
-  .get(funtion(req, res){
-    var data = {data: 'Hello Sanjay'}
-    res.json(data)
-  })
+eventRouter.route("/events").get(function (req, res) {
+  var data = { data: "Hello Sanjay" };
+  res.json(data);
+});
 
-app.use('/api', eventRouter)
+app.use("/api", eventRouter);
+
+app.listen(port, function () {
+  console.log("Running on " + port);
+});
